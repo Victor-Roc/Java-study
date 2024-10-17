941b2682d0a9c4df234b070ea7c2cb52e9852d71
Victor-Roc <ts0598489@gmail.com>
2024-10-17 08:48:29 -0300

    Add files via upload

---
+++ ExeploModulo.Java.java
@@ -0,0 +1,14 @@
+public class Main{
+public static void main(String[]args){
+    System.out.println("antes de chamar o metodo");
+    imprimirInformacoes();
+    System.out.println("depois de chamar o metodo");
+}
+public static void imprimirInformacoes(){
+    System.out.println("=============================");
+    System.out.println("Logica de Programção");
+    System.out.println("Aprendendo a Programar");
+    System.out.println("Like a Boss");
+    System.out.println("==============================");
+}
+}
