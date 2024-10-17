1c4a4cc63e3db352b3ab5726aba0e024886f8673
Victor-Roc <ts0598489@gmail.com>
2024-10-17 09:10:35 -0300

    Add files via upload

---
+++ MensagemModulo.java
@@ -0,0 +1,14 @@
+public class MensagemModulo{
+public static void main(String[]args){
+    
+    mensagem();
+    mensagem();
+    mensagem();
+    
+}
+public static void mensagem(){
+    
+    System.out.println("Meu primeiro programa com funçoes");
+    
+    }
+}
